66fb678a51f969efc716590970663bb088d3ff8f
qpzm7903 <yx372114301@qq.com>
2021-02-28 21:06:10 +0800

    change pom config and add parameter

--- src/main/java/com/qpzm790/HelloWorld.java
+++ src/main/java/com/qpzm790/HelloWorld.java
@@ -4,10 +4,15 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 @Mojo(name = "hello")
 public class HelloWorld extends AbstractMojo {
+    @Parameter(property = "hello.greeting", defaultValue = "hello , maven plugin world")
+    private String greeting;
+
     public void execute() throws MojoExecutionException, MojoFailureException {
-        getLog().info("hello maven plugin world");
+
+        getLog().info(greeting);
     }
 }
